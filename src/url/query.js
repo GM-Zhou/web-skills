f605afda6b98429ea685e4aad05dfe274eaf36ec
zhougm <guangmeng.zhou@ximalaya.com>
2024-12-08 22:44:48 +0800

    feat: 完善 query test

--- src/url/query.js
+++ src/url/query.js
@@ -1,20 +1,31 @@
 /**
  * 获取url参数
- * @param {*} url
+ * @param {string} url
  * @returns
  */
 export const getQuery = (url) => {
-  const result = {};
-  const search = url.split("?")[1];
-  if (search) {
-    search.split("&").forEach((ele) => {
-      try {
-        const [key, value] = ele.split("=");
-        result[key] = JSON.parse(decodeURIComponent(value));
-      } catch (error) {
-        console.error(error);
-      }
-    });
-  }
-  return result;
+	const result = {};
+	const search = url.split('?')[1];
+	if (search) {
+		search.split('&').forEach((ele) => {
+			const [key, value] = ele.split('=');
+			try {
+				result[key] = decode(JSON.parse(value));
+			} catch {
+				result[key] = decode(value);
+			}
+		});
+	}
+	return result;
 };
+
+const decode = (param) => {
+	if (typeof param !== 'object') return decodeURIComponent(param);
+	if (Array.isArray(param)) return param.map(decode);
+	const result = {};
+	for (const key in param) {
+		result[key] = decode(param[key]);
+	}
+	return result;
+};
+
--- src/url/query.test.js
+++ src/url/query.test.js
@@ -3,14 +3,51 @@
 import { getQuery } from './query';
 
 test('getQuery', () => {
-	const url = 'https://www.baidu.com/?a=1&b=2&c=3';
+	const url =
+		'https://www.baidu.com/?a=1&b=2&c=3&rsv_iqid=0xabcf402e01cd47a9&issp=1&f=8&rsv_bp=1&rsv_idx=2&ie=utf-8&rqlang=&tn=baiduhome_pg&ch=&rsv_enter=0&rsv_btype=i&rsv_dl=ib&inputT=1113';
 	const result = getQuery(url);
-	expect(result).toEqual({ a: 1, b: 2, c: 3 });
+	expect(result.rsv_iqid).toEqual('0xabcf402e01cd47a9');
 });
 
 // 更复杂的用例，参数包含中文、回跳 www.baidu.com 并带参数、特殊字符、空格、数字、bool 等
 test('multiple', () => {
-	const url = 'https://www.baidu.com/?a=1&b=2&c=%E6%B5%8B%E8%AF%95&d=%E7%9B%AE%E6%8E%A5&url=%2F';
+	const target =
+		'https://zhidao.baidu.com/question/504901145736526124.html?qbl=relate_question_0&word=%E9%98%BF%E6%96%AF%E8%92%82%E8%8A%AC';
+	const url = `https://www.baidu.com/s?wd=%E9%98%BF%E6%96%AF%E8%92%82%E8%8A%AC&rsv_spt=1&url=${encodeURIComponent(target)}&rsv_iqid=0xabcf402e01cd47a9&issp=1&f=8&rsv_bp=1&rsv_idx=2&ie=utf-8&rqlang=&tn=baiduhome_pg&ch=&rsv_enter=0&rsv_btype=i&rsv_dl=ib&inputT=1113`;
 	const result = getQuery(url);
-	expect(result).toEqual({ a: 1, b: 2, c: '测试', d: '目接', url: '/' });
+	expect(result.url).toEqual(target);
+	expect(result.rsv_iqid).toEqual('0xabcf402e01cd47a9');
+});
+
+describe('getQuery', () => {
+	it('should return empty object when url is empty', () => {
+		expect(getQuery('')).toEqual({});
+	});
+	it('should return empty object when url is not include ?', () => {
+		expect(getQuery('https://www.baidu.com')).toEqual({});
+	});
+	it('should return empty object when url is not include ? and string is empty', () => {
+		expect(getQuery('https://www.baidu.com?')).toEqual({});
+	});
+	it('should return empty object when url is not include ? and string is not string', () => {
+		expect(getQuery('https://www.baidu.com?=' + JSON.stringify({}))).toEqual({});
+	});
+	it('should return object when url is include ?', () => {
+		expect(getQuery('https://www.baidu.com?name=zgm&age=18')).toEqual({
+			name: 'zgm',
+			age: '18',
+		});
+	});
+	it('should return object when url is include ? and string is object', () => {
+		expect(getQuery('https://www.baidu.com?name=zgm&age=' + JSON.stringify({ age: 18 }))).toEqual({
+			name: 'zgm',
+			age: { age: 18 },
+		});
+	});
+	it('should return object when url is include ? and string is array', () => {
+		expect(getQuery('https://www.baidu.com?name=zgm&age=' + JSON.stringify([1, 2, 3]))).toEqual({
+			name: 'zgm',
+			age: [1, 2, 3],
+		});
+	});
 });
